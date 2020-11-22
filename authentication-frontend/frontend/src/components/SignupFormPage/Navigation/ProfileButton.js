4105bdbdcdc794049231c6e36c50558997040f4c
david griffin <65681542+davidleegriffin@users.noreply.github.com>
2020-11-22 13:33:26 -0600

    Update ProfileButton.js

--- authentication-frontend/frontend/src/components/SignupFormPage/Navigation/ProfileButton.js
+++ authentication-frontend/frontend/src/components/SignupFormPage/Navigation/ProfileButton.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../../store/session';
+import './Navigation.css';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -30,8 +31,8 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
+      <button onClick={openMenu} className="userIcon">
+        <i class="fas fa-coins"></i>
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
