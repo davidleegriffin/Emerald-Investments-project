a107f05cff09c32a2321db4f678f44b37523d842
davidleegriffin <davidleegriffin@yahoo.com>
2021-02-08 23:33:39 -0600

    checkup

--- frontend/src/components/WatchList/PortfolioDetail.js
+++ frontend/src/components/WatchList/PortfolioDetail.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './WatchList.css';
 
 function PortfolioDetail(props) {
-  // console.log('portfolioDetail', props.stocks)
-  if (props.stocks === 'undefined') {
+  console.log('portfolioDetail', props.stock)
+  if (props.stock === 'undefined') {
     return ('');
   } else {
     return (
       <div className="portfolio-container">
         <div>
-          <div className="stock-symbol-portfolio">{props.stocks.toUpperCase()}</div>
+          <div className="stock-symbol-portfolio">{props.stock.toUpperCase()}</div>
         </div>
       </div>
     )
