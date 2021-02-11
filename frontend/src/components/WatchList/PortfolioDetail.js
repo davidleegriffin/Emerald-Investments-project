53fc0828ce636f4c7f5cccb31813efbda17855ac
= <davidleegriffin@yahoo.com>
2021-02-11 17:24:23 -0600

    various bug hunts

--- frontend/src/components/WatchList/PortfolioDetail.js
+++ frontend/src/components/WatchList/PortfolioDetail.js
@@ -4,7 +4,7 @@ import { Line } from "react-chartjs-2";
 
 function PortfolioDetail(props) {
   const [data, setData] = useState([]);
-  console.log('portfolioDetail', props);
+  console.log('portfolioDetail', props.stock);
 
   useEffect(() => {
     const stockFetch = async () => {
--- frontend/src/components/WatchList/PortfolioList.js
+++ frontend/src/components/WatchList/PortfolioList.js
@@ -4,23 +4,26 @@ import { useSelector } from "react-redux";
 import './WatchList.css';
 
 
-const PortfolioList = (props) => {
-  // const [portfolio, setPortfolio] = useState();
-  const portfolio = useSelector(action => action.payload);
+const PortfolioList = () => {
+  const statePortfolio = useSelector(state => state.portfolio.portfolio);
+  // console.log("statePortfolio", statePortfolio);
   let arrPortfolio = [];
-  const stocks = (props.portfolio);
-  console.log("props", (props));
-  for (let sym in stocks) {
-    // console.log("sym", sym);
-    arrPortfolio.push(sym);
-  }
-  console.log("arrPortfolio", portfolio);
 
-    return (
-      <div className="mover-container">
-        <div>{arrPortfolio.map((stock,idx) => <PortfolioDetail key={idx} stock={stock} />)}</div>
-      </div>
-    );
+  if (statePortfolio) {
+    for (let i = 0; i < statePortfolio.length; i++) {
+      console.log("statePortfolio", statePortfolio[i].stockSymbol); 
+      if (arrPortfolio.includes(statePortfolio[i].stockSymbol)) {
+        continue;
+      } 
+      arrPortfolio.push(statePortfolio[i].stockSymbol);
+    }  
+  }
+
+  return (
+    <div className="mover-container">
+      <div>{arrPortfolio.map((stock,idx) => <PortfolioDetail key={idx} stock={stock} />)}</div>
+    </div>
+  );
 };
 
 export default PortfolioList;
